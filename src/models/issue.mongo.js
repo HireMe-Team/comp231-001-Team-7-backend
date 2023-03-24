ed8a1c087377dc1135504ce9d522938110df64e4
Joe5986 <yzhou193@my.centennialcollege.ca>
2023-03-23 23:58:09 -0400

    issue Schema added
    issue schema added, need to refer to message schema for replied message ID

---
+++ src/models/issue.mongo.js
@@ -0,0 +1,47 @@
+let mongoose = require('mongoose');
+
+//issues schema
+let applicationModel = mongoose.Schema({
+    issueID:{
+        type:Number,
+        default: '',
+        unique: true,
+        required: true,
+    },
+    title: { //issue title
+        type: String,
+        default: '',
+        required: true
+    },
+    userId: { // the userID of the issue reporter
+        type: Number,
+        default: '',
+        required: true
+    },
+    issueDetail: { // the detail description of the issue
+        type: String,
+        default: '',
+        required: true,
+    },
+    status: { //pending, replied, or closed
+        type: String,
+        default: '',
+        required: 'status is required'
+    },
+    reportDate: {
+        type: Date,
+        default: '',
+        required: true
+    },
+    replyMessage:{ // the unique id for a message, refer to the schema of messages
+        type: Number,
+        default: '',
+        required: false
+    }
+},
+{
+    collection:"applications"
+}
+);
+
+module.exports.Application = mongoose.model('application', applicationModel);
